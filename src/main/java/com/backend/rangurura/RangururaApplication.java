8034bddc019cd07ec65ae7b3da5bf5887c5a00d7
ishimweemmy <ishimweemmy24@gmail.com>
2023-12-22 09:14:29 +0200

    Initial

---
+++ src/main/java/com/backend/rangurura/RangururaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.rangurura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangururaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangururaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/rangurura/RangururaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.rangurura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangururaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
