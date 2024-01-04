dbeb645c98175e46f357e9c391aaf6bea762139d
re-gis <irumvaregisdmc@gmail.com>
2024-01-04 21:41:54 +0200

    Adding the Mapper for the dto

--- src/main/java/com/backend/rangurura/Controllers/ProblemController.java
+++ src/main/java/com/backend/rangurura/Controllers/ProblemController.java
@@ -4,15 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.backend.rangurura.dtos.CreateProblemDto;
 import com.backend.rangurura.response.ApiResponse;
 import com.backend.rangurura.serviceImpl.ProblemServiceImpl;
+import com.backend.rangurura.utils.Mapper;
 import com.backend.rangurura.utils.ResponseHandler;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,9 +23,11 @@ public class ProblemController {
     private final ProblemServiceImpl problemServiceImpl;
 
     @PostMapping("/create")
-    public ResponseEntity<ApiResponse<Object>> PostSuggestion(@Valid @RequestBody CreateProblemDto dto)
+    public ResponseEntity<ApiResponse<Object>> createProblem(@RequestParam("proof") MultipartFile proof,
+            @RequestParam("record") MultipartFile record, @RequestParam("details") String details)
             throws Exception {
         try {
+            CreateProblemDto dto = Mapper.createProblemDto(details, proof, record);
             Object ob = problemServiceImpl.createAProblem(dto);
             return ResponseHandler.success(ob, HttpStatus.CREATED);
         } catch (Exception e) {
