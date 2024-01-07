bebcb9b07c2f584dca2f16c9b1a5eba0736cec16
Edouard <sibomanaedouard974@gmail.com>
2024-01-07 08:12:34 +0200

    swagger

--- src/main/java/com/backend/rangurura/entities/Leaders.java
+++ src/main/java/com/backend/rangurura/entities/Leaders.java
@@ -1,14 +1,19 @@
-package com.backend.rangurura.entities;
-
+package com.backend.rangurura.entities;//package com.backend.rangurura.entities;
+//
 import com.backend.rangurura.enums.ECategory;
 import com.backend.rangurura.enums.EUrwego;
 import com.backend.rangurura.enums.URole;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import javax.persistence.*;
+
+
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "leaders")
@@ -40,3 +45,32 @@ public class Leaders {
 //    private boolean verified;
 
 }
+
+
+
+//import lombok.*;
+//
+//import javax.persistence.*;
+
+//@Entity
+//@Table(name = "patient")
+//@Data
+//public class Patient {
+//    @Id
+//    @Column(name = "patient_id", length = 45)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private int patientID;
+//
+//    @Column(name = "patient_name", length = 100, nullable = false)
+//    private String patientName;
+//
+//    @Column(name = "patient_address", length = 150, nullable = false)
+//    private String patientAddress;
+//
+//    @Column(name = "contact_number", length = 11)
+//    private String contactNumber;
+//
+//    @Column(name = "patient_disease")
+//    private String patientDisease;
+//
+//}
