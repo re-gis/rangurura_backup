bbc2fd5dbe5002e12ada300b1b9c2d8a54c5806a
Edouard <sibomanaedouard974@gmail.com>
2024-01-26 11:06:45 +0200

    rm conflicts

---
+++ src/main/java/com/backend/rangurura/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package com.backend.rangurura;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(" RANGURURA APIs",
+                "REST APIs for RANGURURA Application",
+                "1.0",
+                "Terms of service",
+                new Contact("SIBOMANA Edouard", "https://www.linkedin.com/in/edouard-sibomana-529698262/", "sibomanaedouard974@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
+
