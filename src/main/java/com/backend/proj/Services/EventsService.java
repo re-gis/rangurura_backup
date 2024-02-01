ec308c435464dd595e4ab5cf62229425b7d1bf3c
Edouard <sibomanaedouard974@gmail.com>
2024-02-01 20:59:24 +0200

    Finishing announcement

---
+++ src/main/java/com/backend/proj/Services/EventsService.java
@@ -0,0 +1,18 @@
+package com.backend.proj.Services;
+
+import com.backend.proj.dtos.CreateEventsDto;
+import com.backend.proj.dtos.UpdateEventDto;
+import com.backend.proj.response.ApiResponse;
+
+public interface EventsService {
+    public ApiResponse<Object> createAEvent(CreateEventsDto dto) throws Exception;
+
+    ApiResponse<Object> updateMyEvent(UpdateEventDto dto, Long id) throws Exception;
+    ApiResponse<Object> deleteMyEvent(Long id) throws Exception;
+    ApiResponse<Object> myRecentEvent() throws Exception;
+//    ApiResponse<Object> receivedEvent() throws Exception;
+
+
+
+
+}
